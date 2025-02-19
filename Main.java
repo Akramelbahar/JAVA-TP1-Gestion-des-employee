1ec48761e3127d106066177f11d3c4c652567ee1
Akramelbahar <113865830+Akramelbahar@users.noreply.github.com>
2025-02-19 16:09:22 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
-import DAO.EmployeeDAOImpl;
-import Model.EmployeeModel;
-import Vue.Vue;
-import Controller.EmployeeController;
+# Basic DHCP configuration
 
-public class Main {
-    public static void main(String[] args) {
-        EmployeeDAOImpl dao = new EmployeeDAOImpl();
-        EmployeeModel model = new EmployeeModel(dao);
-        Vue view = new Vue();
+option domain-name "example.com";
+option domain-name-servers 8.8.8.8, 8.8.4.4;  # Google's DNS servers
 
-        // Pass the model and view to the controller
-        new EmployeeController(model, view);
-    }
+default-lease-time 600;
+max-lease-time 7200;
+
+# Subnet definition for 10.0.2.0/24 network (This is typically used in NAT mode in VirtualBox)
+subnet 10.0.2.0 netmask 255.255.255.0 {
+    range 10.0.2.100 10.0.2.200;  # Assign IPs between 10.0.2.100 and 10.0.2.200
+    option routers 10.0.2.2;      # Gateway IP (this is the VirtualBox NAT router IP)
+    option broadcast-address 10.0.2.255;  # Broadcast address
+    option domain-name-servers 8.8.8.8, 8.8.4.4;
 }
